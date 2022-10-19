fb36c3a1a51ee401ce316c0838f351799bf8aeb9
Anoop <kaur0550@algonquinlive.com>
2022-10-19 18:32:00 -0400

    added ratings

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,7 @@
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
 
 
 function Card({data}) {
@@ -9,11 +10,13 @@ function Card({data}) {
     if(data.length === 0) return null;else{
         let result = data.filter(obj => obj.category === "men's clothing")
     return(
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
+        <Grid container spacing={2 }>
         {result.map((item) => (
-          <Grid item xs={2} sm={4} md={4} key={item.id}>
+          <Grid item xs={12} sm={6} md={4} key={item.id}>
             <Paper className="paper">
                 <img src={item.image} alt="" className="img" />
+                <br />
+                <Rating name="disabled" value={item.rating.rate} disabled precision={0.5} />
                 <Typography variant="body1" gutterBottom margin={2}>
                     {item.title}
                    </Typography> 
